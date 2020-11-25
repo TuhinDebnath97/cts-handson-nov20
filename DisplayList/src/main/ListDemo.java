9e7f9b073a199953b2a118b28a1569a3c9026979
Tuhin <tuhindebnath97@gmail.com>
2020-11-25 08:42:18 +0530

    Added display List hands on

---
+++ DisplayList/src/main/ListDemo.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+	public static void main(String args[]) {
+
+	List<User> userdata = new ArrayList<User>();
+	userdata.add(new User(1,"Tuhin","hello123"));
+	userdata.add(new User(2,"Sankhayan","hola123"));
+	userdata.add(new User(3,"Sayak","sdd123"));
+	userdata.add(new User(4,"Ritwik","sdadlo123"));
+	userdata.add(new User(5,"Rounak","dslo123"));
+	
+	for(User user: userdata) {
+		System.out.println("Id: "+user.getId()+" Name: "+user.getName());
+	}
+}
+}
---
+++ DisplayList/src/module-info.java
@@ -0,0 +1,2 @@
+module DisplayList {
+}
